4528e0b767f0af4069b6c57c9f9f7e49837ae62b
Wiz2k20git <marcbrcx@gmail.com>
2023-01-16 15:11:57 -0300

    CI/CD 160123C

--- cicd/3_after.sh
+++ cicd/3_after.sh
@@ -1,4 +1,5 @@
 echo "" >> /var/www/projectdev.services/html/deploy_pd2022f-py.txt
 echo "$(TZ=UTC+3 date +"%d/%m/%Y %H:%M:%S") . Unzip venvlinux" >> /var/www/projectdev.services/html/deploy_pd2022f-py.txt
+cd /var/www/pd2022f-py
 sudo unzip /var/www/pd2022f-py/venvlinux.zip
 echo "$(TZ=UTC+3 date +"%d/%m/%Y %H:%M:%S") . Unzip venvlinux |end|" >> /var/www/projectdev.services/html/deploy_pd2022f-py.txt
